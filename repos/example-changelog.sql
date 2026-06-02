04ec424d6f64f62a6a28035a6c2ba0fa40a5e9ed
Alexandr <nook17n1@gmail.com>
2026-06-02 17:15:56 +0300

    Solution1

--- repos/example-changelog.sql
+++ repos/example-changelog.sql
@@ -51,4 +51,4 @@ CREATE TABLE Users (
 
 --changeset mate.acamemy:6 labels:0.0.3
 CREATE INDEX Email_index ON Users (Email);
---rollback DROP INDEX Email_index ON Users;;
+--rollback DROP INDEX Email_index ON Users;
